1778b711400949550500e1efc59d73eb9dc2c038
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-07 12:16:07 +0900

    프로젝트 세팅

---
+++ src/main/java/kernel360/devinside/DevInsideApplication.java
@@ -0,0 +1,13 @@
+package kernel360.devinside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevInsideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevInsideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kernel360/devinside/DevInsideApplicationTests.java
@@ -0,0 +1,13 @@
+package kernel360.devinside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevInsideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
