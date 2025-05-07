a79b7ee0b73c610aba04acc152628d5f66065403
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-07 22:32:29 +0900

    Security 설정, JwtFilter 추가

---
+++ src/main/java/kernel360/devinside/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package kernel360.devinside.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kernel360.devinside.domain.user.exception.CustomAccessDeniedHandler;
+import kernel360.devinside.domain.user.exception.CustomAuthenticationEntryPoint;
+import kernel360.devinside.domain.user.filter.JwtAuthFilter;
+import kernel360.devinside.domain.user.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // CORS 설정
+        http.cors(cors -> cors.configurationSource(configurationSource()));
+
+        // CSRF 비활성화 (보통 REST API는 CSRF 비활성화)
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // Form Login 비활성화 (프론트에서 로그인 처리할 경우 주로 비활성화)
+        http.formLogin(AbstractHttpConfigurer::disable);
+
+        // HTTP Basic 인증 비활성화 (JWT 사용 시 필요 없음)
+        http.httpBasic(AbstractHttpConfigurer::disable);
+
+        // 경로별 인가 작업
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/user/**").permitAll()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**","/actuator/**").permitAll()
+                .anyRequest().authenticated());
+
+        // 세션 설정 : STATELESS
+        http.sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // JWT 필터 추가
+        http.addFilterBefore(new JwtAuthFilter(jwtUtil, objectMapper), UsernamePasswordAuthenticationFilter.class);
+
+        // Exception handler 추가
+        http.exceptionHandling((exceptionHandling) -> exceptionHandling
+                .accessDeniedHandler(new CustomAccessDeniedHandler(objectMapper))
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint(objectMapper)));
+
+        return http.build();
+    }
+
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowedOrigins(Arrays.asList("https://", "http://localhost:5173", "https://","https://"));
+        configuration.setAllowCredentials(true);
+        configuration.addExposedHeader("ACCESS_TOKEN");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
